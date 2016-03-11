9ee93d742787942fa7966a1e8f833e44ec0c7394
Cristian Tala S. <cristian.tala@gmail.com>
2016-03-11 15:25:37 -0300

    Funciona con Modo devel

--- WooBoPolis.php
+++ WooBoPolis.php
@@ -5,7 +5,7 @@ namespace WooBoPolis;
 /*
   Plugin Name: WooBoPolis
   Plugin URI:  https://github.com/ctala/WooBoPolis
-  Description: Base para la Creación de otros plugins
+  Description: Este plugin envía la información de una orden completa a CuboPolis.
   Version:     1.0
   Author:      Cristian Tala Sánchez
   Author URI:  http://www.cristiantala.cl
@@ -14,7 +14,7 @@ namespace WooBoPolis;
   Domain Path: /languages
   Text Domain: ctala-text_domain
  */
-//include_once 'helpers/debug.php';
+
 include_once 'vendor/autoload.php';
 
 use WooBoPolis\classes\Logger;
@@ -29,7 +29,12 @@ function woocommerce_order_status_completed($order_id) {
     //Tokens
     $cubopolisTRX->set_access_token(get_option("cubopolis_access_token"));
     $cubopolisTRX->set_resource_token(get_option("cubopolis_resource_token"));
-    $cubopolisTRX->set_CUBOPOLISAPIURL("http://cubopolis.dev/v1/api/");
+    
+    if("on" === get_option("cubopolis_desarrollo_toggle"))
+    {
+        $cubopolisTRX->set_CUBOPOLISAPIURL(get_option("cubopolis_desarrollo_url"));
+    }
+    
     
     //Obtengo la Orden de Woocommerce
     $order = new \WC_Order($order_id);
--- views/SettingsMenu.php
+++ views/SettingsMenu.php
@@ -17,13 +17,19 @@ class SettingsMenu {
             update_option("cubopolis_access_token", $access_token);
             update_option("cubopolis_resource_token", $resource_token);
         }
+
+        if (isset($_POST["desarrollo"])) {
+            $desarrollo_toggle = esc_attr($_POST["desarrollo_toggle"]);
+            $desarrollo_url = esc_attr($_POST["desarrollo_url"]);
+
+            update_option("cubopolis_desarrollo_toggle", $desarrollo_toggle);
+            update_option("cubopolis_desarrollo_url", $desarrollo_url);
+        }
         ?>
         <div class="wrap">
 
             <div class="wrap">
                 <h2>CuboPolis Settings</h2>
-
-
 
                 <form method="POST" action="">
                     <table class="form-table">
@@ -49,6 +55,39 @@ class SettingsMenu {
                         </tr>
                     </table>
                     <input type="submit" name="actualizar" value="Actualizar Datos">
+                </form>
+            </div>
+            <hr>
+
+
+            <div class="wrap">
+                <h3>Modo Desarrollo</h3>
+
+                <form method="POST" action="">
+                    <table class="form-table">
+                        <tr valign="top">
+                            <th scope="row">
+                                <label for="num_elements">
+                                    Habilitar Desarrollo:
+                                </label> 
+                            </th>
+                            <td>
+                                 
+                                <input type="checkbox" name="desarrollo_toggle" <?php echo ("on" === get_option("cubopolis_desarrollo_toggle") ? "checked" : ""); ?>><br>
+                            </td>
+                        </tr>
+                        <tr valign="top">
+                            <th scope="row">
+                                <label for="num_elements">
+                                    Desarrollo URL:
+                                </label> 
+                            </th>
+                            <td>
+                                <input type="text" name="desarrollo_url" size="100" value="<?php echo get_option("cubopolis_desarrollo_url") ?>"/>
+                            </td>
+                        </tr>
+                    </table>
+                    <input type="submit" name="desarrollo" value="Actualizar Datos Desarrollo">
                 </form>
             </div>
             <?php
